e165fb556f8d1b45c0bb56ad85add9b0521b9166
Nathan N <Nitharios@users.noreply.github.com>
2017-08-17 19:45:27 -1000

    Edited sumAllPositive function

--- exercises.js
+++ exercises.js
@@ -260,7 +260,17 @@ var getKeys = function(obj) {
  * @param {String}
  * @return {Number}
  */
-var sumAllPositive;
+var sumAllPositive = function(arr) {
+  result = 0
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      result += arr[i]
+    }
+  }
+
+  return result
+}
 
 /* #stringCountBelowThree
  *
